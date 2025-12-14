a53097b2903934de7034b8b677d2dcd8e1a13533
EncryptedSushi <jsaitoh@gmail.com>
2025-12-14 18:01:00 +0900

    Adding ripgreprc to ignore windows system files, among other things like node_modules.  Defined  to make ripgrep respect it.

---
+++ readonly_Documents/PowerShell/config/environment.ps1
@@ -0,0 +1,3 @@
+# XDG_CONFIG_HOME should be set via the GUI, but uncomment the below if not
+# $env:XDG_CONFIG_HOME = "$HOME\.config"
+$env:RIPGREP_CONFIG_PATH = $env:XDG_CONFIG_HOME\ripgrep\.ripgreprc"
