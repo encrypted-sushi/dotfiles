8a0970e3b1956f8c2c8c46fd009d43ab38392b6b
EncryptedSushi <jsaitoh@gmail.com>
2025-12-14 14:51:39 +0900

    Add westerm config mudles) appearance, fonts, general, keymaps)

---
+++ dot_config/wezterm/configs/fonts.lua
@@ -0,0 +1,35 @@
+-- %USERPROFILE%\.config\wezterm\configs\fonts.lua
+-- Font configuration with fallbacks and italic rules
+
+local M = {}
+
+function M.setup(config, wezterm)
+  -- Global font size
+  config.font_size = 15.0
+
+  -- Primary font with fallbacks
+  config.font = wezterm.font_with_fallback {
+    { 
+      family = "VictorMono Nerd Font", 
+      weight = "Medium",
+      harfbuzz_features = {
+        "liga=1",
+        "calt=1",
+        "ss04",    
+      },
+    },
+    
+    -- Fallback: CJK Font (Sarasa Gothic TTC Unhinted)
+    { family = "Sarasa Gothic Ttc", weight = "Regular" }, 
+  }
+
+  -- Cursive italic blend (Victor Mono for italic text)
+  config.font_rules = {
+    {
+      italic = true,
+      font = wezterm.font("VictorMono Nerd Font", { style = "Italic" }),
+    },
+  }
+end
+
+return M
