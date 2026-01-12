88e762fea6a16a7cae076144e3a5fdd474ed38a9
EncryptedSushi <jsaitoh@gmail.com>
2026-01-12 15:46:24 +0900

    added scrollback issue workaround configs to scroll.lua after running neovim

--- dot_config/wezterm/configs/behaviors/scroll.lua
+++ dot_config/wezterm/configs/behaviors/scroll.lua
@@ -10,6 +10,16 @@ function M.setup(config)
 
   -- Ensure alternate screen scrollback is preserved (can't scroll after Neovim fix)
   config.alternate_buffer_wheel_scroll_speed = 1
+
+  -- ADD THIS: 
+  -- If you find yourself stuck, holding SHIFT while scrolling 
+  -- will bypass any app logic and force the terminal to scroll.
+  config.bypass_mouse_reporting_modifiers = 'SHIFT'
+  
+  -- ADD THIS (Windows Specific):
+  -- Helps prevent the alternate screen from "eating" the scrollback
+  -- when switching between pwsh and TUIs.
+  config.hide_mouse_cursor_when_typing = false
 end
 
 return M
