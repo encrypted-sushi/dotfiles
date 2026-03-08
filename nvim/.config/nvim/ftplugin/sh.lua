70c30738b7e8b35e3c6cae3b536525de9de1ccee
EncryptedSushi <jsaitoh@gmail.com>
2026-03-08 18:15:34 +0900

    updated configs

--- nvim/.config/nvim/ftplugin/sh.lua
+++ nvim/.config/nvim/ftplugin/sh.lua
@@ -2,3 +2,18 @@
 vim.treesitter.language.register("bash", "sh")
 vim.bo.syntax = "off"
 vim.treesitter.start()
+
+vim.lsp.start({
+    name = "efm",
+    --cmd = { "efm-langserver" },
+    cmd = { "efm-langserver", "-logfile", "/tmp/efm.log", "-loglevel", "5" },
+    filetypes = { "sh" },
+    root_dir = vim.fn.getcwd(),
+    init_options = {
+        documentFormatting = false,
+        hover = false,
+        documentSymbol = false,
+        codeAction = false,
+        completion = false,
+    },
+})
