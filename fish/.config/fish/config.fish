af193627e2af72a8c2dc77cfdf36871bf1ea6958
EncryptedSushi <jsaitoh@gmail.com>
2026-03-24 01:44:22 +0900

    add ssh relay and local config sourcing

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,7 @@
-if status is-interactive
-# Commands to run in interactive sessions can go here
-  
-  set -x SSH_AUTH_SOCK $HOME/.ssh/agent.sock
+# ~/.config/fish/config.fish
+set -gx SSH_AUTH_SOCK $HOME/.ssh/agent.sock
 
-  # Only start if not already running
-  if not test -S "$SSH_AUTH_SOCK"
-    rm -f "$SSH_AUTH_SOCK"
-    setsid socat UNIX-LISTEN:"$SSH_AUTH_SOCK,fork" \
-        EXEC:"$HOME/.local/bin/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &
-  end
+# Source a local, machine-specific file if it exists (not in dotfiles)
+if test -f $HOME/.config/fish/local.fish
+    source $HOME/.config/fish/local.fish
 end
